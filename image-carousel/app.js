0a88521bf9bb2e5ac272cec1bb5514764b9001df
alfattahdicky <alfattahdicky2@gmail.com>
2021-08-22 17:47:58 +0700

    finish project

--- image-carousel/app.js
+++ image-carousel/app.js
@@ -5,12 +5,26 @@ const imgs = document.querySelectorAll('img');
 
 let index = 0;
 
-rightEl.addEventListener('click', function (e) {
-  if (index < imgs.length) {
-    imgs[index].style.display = 'none';
-    index++;
-  } else if (index === imgs.length) {
-    index = 0;
-    imgs[index].style.display = 'block';
-  };
-});
+
+function slideImage() {
+  for (let i = 0; i < imgs.length; i++) imgs[i].style.display = 'none';
+
+  if (index > imgs.length - 1) index = 0;
+  if (index < 0) index = imgs.length - 1;
+
+  imgs[index].style.display = 'block';
+}
+
+function nextImage() {
+  index++;
+  slideImage();
+}
+
+function previousImage() {
+  index--;
+  slideImage();
+}
+
+
+leftEl.addEventListener('click', previousImage);
+rightEl.addEventListener('click', nextImage);
