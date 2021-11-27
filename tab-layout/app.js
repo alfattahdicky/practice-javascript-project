99033902ce68ddbf79b6e92daf423008ecf527bd
alfattahdicky <alfattahdicky2@gmail.com>
2021-11-27 10:01:38 +0700

    finish project tab-layout

--- tab-layout/app.js
+++ tab-layout/app.js
@@ -1,11 +1,23 @@
 const tabLinks = document.querySelectorAll(".tab-link");
 const tabContent = document.querySelectorAll(".content");
+const tabActive = document.querySelector('.tab-active');
+const tab = document.querySelector('.tab');
+const offsetWidthTab = tab.offsetWidth;
 
 for (let i = 0; i < tabLinks.length; i++) {
   tabLinks[i].addEventListener("click", function () {
+    for(let i = 0; i < tabLinks.length; i++) {
+      tabLinks[i].classList.remove('active');
+    }
     for (let i = 0; i < tabContent.length; i++) {
       tabContent[i].classList.remove("active");
     }
+
+    tabLinks[i].classList.add('active');
     tabContent[i].classList.add("active");
+
+    if(tabLinks[i].classList.contains('active')) {
+      tabActive.style.transform = `translateX(${i * (offsetWidthTab / 2 - 30)}px)`;
+    }
   });
 }
